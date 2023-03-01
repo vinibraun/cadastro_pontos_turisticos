303eb61ec3633ea3513469a2c1b58f6a732adf98
Giovani Marcarini <inf_giovani@hotmail.com>
2023-03-01 19:45:46 -0300

    Initial

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'App - Gerenciador de tarefas',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Gerenciador de Tarefas'),
     );
   }
 }
@@ -49,15 +49,19 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _numero = 0;
 
   void _incrementCounter() {
+    _counter++;
+    _numero = (_counter * 2);
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter;
+      //_numero;
     });
   }
 
@@ -96,10 +100,17 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Nùmero de vezes que o icon + foi precionado',
             ),
             Text(
               '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const Text(
+              'resultado do contador multiplicado por 2',
+            ),
+            Text(
+              '$_numero',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
