8fa114da57a0d0f5a621859867139101b5c477b6
Giovani Marcarini <inf_giovani@hotmail.com>
2023-03-07 20:49:09 -0300

    commit aula 07-03

--- lib/pages/lista_tarefas_page.dart
+++ lib/pages/lista_tarefas_page.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gerenciador_tarefas_md/model/tarefa.dart';
 
 class ListaTarefaPage extends StatefulWidget{
 
@@ -8,22 +9,26 @@ class ListaTarefaPage extends StatefulWidget{
   _ListaTarefasPageState createState() => _ListaTarefasPageState();
 
 }
-
 class _ListaTarefasPageState extends State<ListaTarefaPage>{
 
+  final tarefas = <Tarefa>
+   [
+     //Tarefa(id: 1,
+     //    descricao: 'Fazer atividades da aula',
+      //    prazo: DateTime.now().add(Duration(days: 5)),
+     //)
+  ];
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: _criarAppBar(),
-      body: null,
+      body: _criarBody(),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Nova tarefa',
-        child: Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
-
-
     );
   }
 
@@ -37,4 +42,24 @@ class _ListaTarefasPageState extends State<ListaTarefaPage>{
       ],
     );
  }
+  Widget _criarBody(){
+    if( tarefas.isEmpty){
+      return const Center(
+        child: Text('Nenhuma tarefa cadastrada',
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      );
+    }
+    return ListView.separated(
+        itemCount: tarefas.length,
+        itemBuilder: (BuildContext context, int index){
+          final tarefa = tarefas[index];
+          return ListTile(
+            title: Text('${tarefa.id} - ${tarefa.descricao}'),
+            subtitle: Text('Prazo - ${tarefa.prazo}'),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) => Divider(),
+        );
+  }
 }
